4b8af011aabac8c6cc1c6cfd007ec6a1bb1fe106
Chaos-Sheeza <jesmar16@live.com>
2018-03-01 11:25:38 +0100

    Added a boolean function that checks if a number is prime

--- Task3.py
+++ Task3.py
@@ -9,5 +9,14 @@ def prime(n):
     return arr
 
 
+def IsPrime(n):
+    for x in range(2, n):
+        if n % x == 0 and n != x:
+            return False
+    return True
+
+
 UserInput = input("Enter a number to find all the prime number up to it\n")
 print("Prime Numbers: \n%s" % prime(UserInput))
+User2 = input("Enter a number to check if it's prime\n")
+print(IsPrime(User2))
