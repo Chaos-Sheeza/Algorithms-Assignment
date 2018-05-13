8a7c7413a406e6956e8c9a8a5a86e5719109ec4d
Chaos-Sheeza <jesmar16@live.com>
2018-05-13 19:19:47 +0200

    Added comments

--- Task3.py
+++ Task3.py
@@ -1,7 +1,13 @@
+# Function that outputs list of prime numbers up to n.
+
+
+# Creates a list from 2 to n, and loop size square root of n (where n is the size of the list). Then divides every
+# element of the list by every number removing every element that does not have a remainder.
 def prime(n):
     arr = list(range(2, n + 1))
     for x in range(int(n**0.5)):
         for i in arr:
+            # Added so that first prime found is not removed
             if arr[x] == i:
                 None
             elif (i % arr[x]) == 0:
@@ -9,6 +15,9 @@ def prime(n):
     return arr
 
 
+# Boolean function to check whether a number is prime.
+# Divides n by every number from 2 and if one does not return a remainder, then the loop is stopped and returns false,
+# else it returns true.
 def IsPrime(n):
     for x in range(2, n):
         if n % x == 0 and n != x:
