7db1dca4418bc4d9cf6368de03a194233a0bcee1
Chaos-Sheeza <jesmar16@live.com>
2018-05-13 16:07:34 +0200

    Added comments, method no longer outputs different permutations of the same answer

--- Task1.py
+++ Task1.py
@@ -1,18 +1,24 @@
-def pairs(liz):
-    for a in range(len(liz)):
-        w = liz[a]
-        for b in range(len(liz)):
-            if liz[b] != w:
-                x = liz[b]
-                for c in range(len(liz)):
-                    if liz[c] != w and liz[c] != x:
-                        y = liz[c]
-                        for d in range(len(liz)):
-                            if liz[d] != w and liz[d] != x and liz[d] != y:
-                                z = liz[d]
-                                if (w*x) == (y*z):
-                                    print("\n(%d,%d),(%d,%d) = %d" % (w, x, y, z, (w*x)))
+# Method grabs values from an array and tries different combinations to find two pairs that ave the same product.
+# Grabs first element, grabs one after it, takes another after the first, that is not equal to the second, the takes one
+# after the third that is also not equal to the second, multiplies them separately and prints them if they have an equal
+# product. Thus avoiding different permutations of the same answer.
 
 
-varx = list(range(1, 1025))
+def pairs(arr):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            for k in range(i+1, len(arr)):
+                if arr[k] != arr[j]:
+                    for m in range(k+1, len(arr)):
+                        if arr[m] != arr[j]:
+                            if (arr[i]*arr[j]) == (arr[k]*arr[m]):
+                                print("%d x %d = %d x %d = %d" % (arr[i], arr[j], arr[k], arr[m], (arr[k]*arr[m])))
+
+
+varx = list(range(1, 11))
 pairs(varx)
+
+# Completely inefficient method. Another method would be to have an object that takes 2 integers and their product.
+# Create an array of said objects using the list of numbers, and then using the same method in task 6, remove the
+# objects from the list that have unique product value, then just print different combinations of the resulting array of
+# objects with repeated product value.
