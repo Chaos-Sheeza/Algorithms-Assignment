045b6996444161d945cdce75de565ec0db7b5f07
Chaos-Sheeza <jesmar16@live.com>
2018-05-17 00:30:59 +0200

    Added comments

--- Task9.py
+++ Task9.py
@@ -1,13 +1,20 @@
+# Iterative sum of fibonacci function.
 def fibi(n):
-    l = [0,1,0]
-    sm = 0
+    lis = [0, 1, 0]             # Array that stores the two latest values of fibonacci and their addition.
+    sm = 0                      # Variable to store the sum.
+    # Loop the size of n, adds first and second and stores result in third element of the array, second element added to
+    # the sum, since technically the second element is the actual last term, the third is the sum of the last and the
+    # term before it.
     for i in range(n):
-        l[2] = l[0] + l[1]
-        sm = (sm+l[1])
-        l[0] = l[1]
-        l[1] = l[2]
+        lis[2] = lis[0] + lis[1]
+        sm = (sm+lis[1])
+        lis[0] = lis[1]
+        lis[1] = lis[2]
     return sm
 
+
+# Recursive fibonacci function. Created to compare efficiency.
+'''
 def fibr(n):
     if n == 0:
         return 0
@@ -16,11 +23,13 @@ def fibr(n):
     else:
         return fibr(n-1)+fibr(n-2)
 
+
 def sumfibr(n):
     res = 0
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         res = res + fibr(i)
     return res
+'''
 
-
-print("Recursive = %d\n" % sumfibr(10) + "Iterative = %d" % fibi(10))
+print("Iterative = %d" % fibi(10))
+# print("Recursive = %d" % sumfibr(10))
