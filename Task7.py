3dadd731cd890204ead955ba6a91fd042c074547
Chaos-Sheeza <jesmar16@live.com>
2018-05-16 13:56:48 +0200

    Added comments

--- Task7.py
+++ Task7.py
@@ -1,6 +1,10 @@
+# Recursive function that find largest number.
 def large(lnum):
+    # Stopping condition when lnum has only one element.
     if len(lnum) == 1:
         return lnum
+    # 3 conditions used. If first element larger than second, remove second, opposite remove first, equal remove
+    # whatever, then calls the function again. By the end there should only be one element left, which is the largest.
     else:
         if lnum[0] > lnum[1]:
             lnum.remove(lnum[1])
