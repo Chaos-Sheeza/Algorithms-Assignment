3a0990045f1199fec69d73579638ad5bd0155755
Chaos-Sheeza <jesmar16@live.com>
2018-05-16 13:40:53 +0200

    Added comments

--- Task6.py
+++ Task6.py
@@ -1,3 +1,9 @@
+# Function creates 2 arrays, one for repeated values, the other for unique values. The function copies an element form
+# the original array and stores it in a temporary variable, checks and removes all the elements in the array that are
+# equal to it, then if the function removed more than 1 elements (using x as a counter), the variable is appended to the
+# array of repeated values. Otherwise the variable is appended to the array of unique values.
+
+
 def rep(lnum):
     lrep, luni = [], []
     while len(lnum) != 0:
@@ -10,8 +16,8 @@ def rep(lnum):
             luni.append(var)
         else:
             lrep.append(var)
-    return "unique = %s" %luni + "\nrepeated = %s" %lrep
+    return "unique = %s" % luni + "\nrepeated = %s" % lrep
 
 
-arr = [1,1,2,3,3,3,2,1,4,5,6,7,8,9,7,6]
+arr = [1, 1, 2, 3, 3, 3, 2, 1, 4, 5, 6, 7, 8, 9, 7, 6]
 print(rep(arr))
